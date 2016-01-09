a56be238139225cc920b25b46c983a3a748928ca
Liran Tal <liran.tal@gmail.com>
2016-01-09 23:23:16 +0200

    adding more tests for sanity safetynet

--- test/test.js
+++ test/test.js
@@ -14,57 +14,139 @@ describe('CRON to Quartz - Test Suite', function () {
 
   describe('Sanity Tests for CRON to Quartz conversion', function() {
 
-    it('should return an auth token', function() {
+    it('CRON @hourly should resolve to a proper quartz configuration', function() {
 
-		a = c.getQuartz('@hourly');
-		console.log(a);
+    	var quartz = C2Q.getQuartz('@hourly');
 
-        should.not.exist(err);
+    	quartz.should.be.an.instanceof(Array).and.have.length(1);
 
-        body.should.be.an.instanceof(Object).and.have.property('access_token');
-        body.should.be.an.instanceof(Object).and.have.property('token_type');
-        body.should.be.an.instanceof(Object).and.have.property('expires_in');
-        body.should.be.an.instanceof(Object).and.have.property('scope');
+    	var quartzConfigArray = quartz.pop();
+    	quartzConfigArray.should.be.an.instanceof(Array).and.have.length(7);
 
-      });
+    	var quartzConfigStr = quartzConfigArray.join(' ');
+		quartzConfigStr.should.be.an.instanceof(String);
+		quartzConfigStr.should.be.equal('0 0 * * * ? *');
+
+    });
+
+    it('CRON 0 0,12 1 */2 * should resolve to a proper quartz configuration', function() {
+
+    	var quartz = C2Q.getQuartz('0 0,12 1 */2 *');
+
+    	quartz.should.be.an.instanceof(Array).and.have.length(1);
+
+    	var quartzConfigArray = quartz.pop();
+    	quartzConfigArray.should.be.an.instanceof(Array).and.have.length(7);
+
+    	var quartzConfigStr = quartzConfigArray.join(' ');
+		quartzConfigStr.should.be.an.instanceof(String);
+		quartzConfigStr.should.be.equal('0 0 0,12 1 */2 ? *');
+
+    });
+
+    it('CRON * * * * * should resolve to a proper quartz configuration', function() {
+
+    	var quartz = C2Q.getQuartz('* * * * *');
+
+    	quartz.should.be.an.instanceof(Array).and.have.length(1);
+
+    	var quartzConfigArray = quartz.pop();
+    	quartzConfigArray.should.be.an.instanceof(Array).and.have.length(7);
+
+    	var quartzConfigStr = quartzConfigArray.join(' ');
+		quartzConfigStr.should.be.an.instanceof(String);
+		quartzConfigStr.should.be.equal('0 * * ? * * *');
+
+    });
+
+    it('CRON 00 11,13 * * * should resolve to a proper quartz configuration', function() {
+
+    	var quartz = C2Q.getQuartz('00 11,13 * * *');
+
+    	quartz.should.be.an.instanceof(Array).and.have.length(1);
+
+    	var quartzConfigArray = quartz.pop();
+    	quartzConfigArray.should.be.an.instanceof(Array).and.have.length(7);
+
+    	var quartzConfigStr = quartzConfigArray.join(' ');
+		quartzConfigStr.should.be.an.instanceof(String);
+		quartzConfigStr.should.be.equal('0 00 11,13 ? * * *');
+
+    });
+
+    it('CRON 00 03-18 * * 1-5 should resolve to a proper quartz configuration', function() {
+
+    	var quartz = C2Q.getQuartz('00 03-18 * * 1-5');
+
+    	quartz.should.be.an.instanceof(Array).and.have.length(1);
+
+    	var quartzConfigArray = quartz.pop();
+    	quartzConfigArray.should.be.an.instanceof(Array).and.have.length(7);
+
+    	var quartzConfigStr = quartzConfigArray.join(' ');
+		quartzConfigStr.should.be.an.instanceof(String);
+		quartzConfigStr.should.be.equal('0 00 03-18 ? * 1-5 *');
+
+    });
+
+    it('CRON */10 * * * * should resolve to a proper quartz configuration', function() {
+
+    	var quartz = C2Q.getQuartz('*/10 * * * *');
+
+    	quartz.should.be.an.instanceof(Array).and.have.length(1);
+
+    	var quartzConfigArray = quartz.pop();
+    	quartzConfigArray.should.be.an.instanceof(Array).and.have.length(7);
+
+    	var quartzConfigStr = quartzConfigArray.join(' ');
+		quartzConfigStr.should.be.an.instanceof(String);
+		quartzConfigStr.should.be.equal('0 */10 * ? * * *');
+
+    });
+
+    it('CRON 0 4 15-21 * 1 should resolve to a proper quartz configuration - supporting and yielding 2 quartz configs', function() {
+
+    	// The following is a complex CRON expression which basically creates an OR between DOM and DOW
+    	// It is sepcialy because it yields 2 arrays for possible values to create for a Quartz scheduler
+    	var quartz = C2Q.getQuartz('0 4 15-21 * 1');
+
+    	quartz.should.be.an.instanceof(Array).and.have.length(2);
+
+    	// Extract array notation 1
+    	var quartzConfigArray2 = quartz.pop();
+    	quartzConfigArray2.should.be.an.instanceof(Array).and.have.length(7);
+
+    	var quartzConfigStr2 = quartzConfigArray2.join(' ');
+		quartzConfigStr2.should.be.an.instanceof(String);
+
+		quartzConfigStr2.should.be.equal('0 0 4 ? * 1 *');
+
+		// Extract array notation 2
+		var quartzConfigArray1 = quartz.pop();
+    	quartzConfigArray1.should.be.an.instanceof(Array).and.have.length(7);
+
+    	var quartzConfigStr1 = quartzConfigArray1.join(' ');
+		quartzConfigStr1.should.be.an.instanceof(String);
+
+		quartzConfigStr1.should.be.equal('0 0 4 15-21 * ? *');
+
+    });
+
+    it('CRON 0 4 8-14 * * should resolve to a proper quartz configuration', function() {
+
+    	var quartz = C2Q.getQuartz('0 4 8-14 * *');
+
+    	quartz.should.be.an.instanceof(Array).and.have.length(1);
+
+    	var quartzConfigArray = quartz.pop();
+    	quartzConfigArray.should.be.an.instanceof(Array).and.have.length(7);
+
+    	var quartzConfigStr = quartzConfigArray.join(' ');
+		quartzConfigStr.should.be.an.instanceof(String);
+		quartzConfigStr.should.be.equal('0 0 4 8-14 * ? *');
+
     });
 
   });
 
 });
-	/*
-	tests:
-
-	cron:   0 0,12 1 *\/2 *
-	quartz: 0 0 0,12 1 *\/2 ? *
-
-	cron:   * * * * *
-	quartz: 0 * * * * ? *
-
-
-	cron: 00 11,13 * * *
-	quartz: 0 00 11,13 * * ? *
-
-	cron: 00 03-18 * * 1-5 
-	qaurtz: 0 00 03-18 ? * 1-5 *
-
-	cron: *\/10 * * * *
-	quartz:
-
-	cron: 0 4 15-21 * 1
-	quartz: 0 0 4 15-21 * ? *
-
-	cron: 0 4 8-14 * *
-	quartz: 0 0 4 8-14 * ? *
-
-
-	*/
-
-	//var str = '0 0,12 1 */2 *';
-	//var str = '* * * * *';
-	//var str = '00 11,13 * * *';
-	//var str = '00 03-18 * * 1-5';
-	//var str = '*/10 * * * *';
-	//var str = '0 4 15-21 * 1';
-	//var str = '0 4 8-14 * *';
-	//var crontab = '0 0 1 1 *';
