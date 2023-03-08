41db40e3a679b88bb795807dd18a32dad4c97826
George <89553456+George-Botron@users.noreply.github.com>
2023-03-08 10:43:50 -0800

    Add correct type to function.

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,8 @@
 declare module 'cron-to-quartz' {
-  function getQuartz(crontab: string): any;
+  /**
+ * The Quartz Scheduler isn't fully compatible with the CRON notation, so while CRON allows logical OR expressions, Quartz doesn't do that. 
+ * For this reason, if you provide such CRON syntax, then the C2Q object will yield an array of 2 values
+ * @type {string}
+ */
+  function getQuartz(crontab: string): Array<T>;
 }
